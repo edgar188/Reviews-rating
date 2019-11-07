7aeeb4bfa2dcc2f88c1951c0a6b110ff9dba379c
edgar <edgar.hovhannisyan08@gmail.com>
2019-11-08 00:09:44 +0400

    new changes

--- js/script.js
+++ js/script.js
@@ -11,69 +11,6 @@ function responsiveNavbar() {
     }
 }
 
-const introSlideData = [
-    {
-        topText: "Reviews Ratings Comparisons",
-        bottomText: "Unbiased Reviews That Save You Time and Money",
-        picture: "vector.png"
-    },
-    {
-        topText: "Top text 2",
-        bottomText: "Bottom text 2",
-        picture: "capture.jpg"
-    },
-    {
-        topText: "Top text 3",
-        bottomText: "Bottom text 3",
-        picture: "soldier.jpg"
-    }
-];
-
-function introSlider(target) {
-    var topText = document.getElementById("top_text"),
-        bottomText = document.getElementById("bottom_text"),
-        heroImg = document.getElementById("hero_img"),
-        imgSource = heroImg.src.slice(heroImg.src.indexOf("/intro/")),
-        currentImg = imgSource.split("/")[2],
-        currImgIndex;
-
-    for (var val of introSlideData) {
-        if (val.picture === currentImg) {
-            currImgIndex = introSlideData.indexOf(val);
-            break;
-        }
-    }
-
-    if(target.id === "next") {
-        if(currentImg === introSlideData[introSlideData.length-1].picture) {
-            heroImg.src = heroImg.src.replace(imgSource, "/intro/" + introSlideData[0].picture);
-            heroImg.alt = introSlideData[0].picture.split(".")[1];
-            topText.innerText = introSlideData[0].topText;
-            bottomText.innerText = introSlideData[0].bottomText;
-        }
-        else {
-            heroImg.src = heroImg.src.replace(imgSource, "/intro/" + introSlideData[currImgIndex + 1].picture);
-            heroImg.alt = introSlideData[0].picture.split(".")[1];
-            topText.innerText = introSlideData[currImgIndex + 1].topText;
-            bottomText.innerText = introSlideData[currImgIndex + 1].bottomText;
-        }
-    } 
-    else {
-        if(currentImg === introSlideData[0].picture) {
-            heroImg.src = heroImg.src.replace(imgSource, "/intro/" + introSlideData[introSlideData.length-1].picture);
-            heroImg.alt = introSlideData[0].picture.split(".")[1];
-            topText.innerText = introSlideData[introSlideData.length-1].topText;
-            bottomText.innerText = introSlideData[introSlideData.length-1].bottomText;
-        } 
-        else {
-            heroImg.src = heroImg.src.replace(imgSource, "/intro/" + introSlideData[currImgIndex - 1].picture);
-            heroImg.alt = introSlideData[0].picture.split(".")[1];
-            topText.innerText = introSlideData[currImgIndex - 1].topText;
-            bottomText.innerText = introSlideData[currImgIndex - 1].bottomText;
-        }
-    }
-}
-
 // ------------------------ Intro slider --------------------------
 $('#owl-demo').owlCarousel({
     loop:true,
@@ -89,6 +26,8 @@ $('.owl-carousel').owlCarousel({
     nav: true,
     dots: true,
     responsiveClass: true,
+    navText: [$('.partners_slider_img_next'),$('.partners_slider_img_prev')],
+    rewindNav : true,
     responsive: {
         0: {
             items: 1,
@@ -117,3 +56,67 @@ function initMap() {
     // The marker, positioned at Uluru
     // var marker = new google.maps.Marker({position: uluru, map: map});
 }
+
+// --------------------------- Slider Intro ---------------------------------
+// const introSlideData = [
+//     {
+//         topText: "Reviews Ratings Comparisons",
+//         bottomText: "Unbiased Reviews That Save You Time and Money",
+//         picture: "vector.png"
+//     },
+//     {
+//         topText: "Top text 2",
+//         bottomText: "Bottom text 2",
+//         picture: "capture.jpg"
+//     },
+//     {
+//         topText: "Top text 3",
+//         bottomText: "Bottom text 3",
+//         picture: "soldier.jpg"
+//     }
+// ];
+
+// function introSlider(target) {
+//     var topText = document.getElementById("top_text"),
+//         bottomText = document.getElementById("bottom_text"),
+//         heroImg = document.getElementById("hero_img"),
+//         imgSource = heroImg.src.slice(heroImg.src.indexOf("/intro/")),
+//         currentImg = imgSource.split("/")[2],
+//         currImgIndex;
+
+//     for (var val of introSlideData) {
+//         if (val.picture === currentImg) {
+//             currImgIndex = introSlideData.indexOf(val);
+//             break;
+//         }
+//     }
+
+//     if(target.id === "next") {
+//         if(currentImg === introSlideData[introSlideData.length-1].picture) {
+//             heroImg.src = heroImg.src.replace(imgSource, "/intro/" + introSlideData[0].picture);
+//             heroImg.alt = introSlideData[0].picture.split(".")[1];
+//             topText.innerText = introSlideData[0].topText;
+//             bottomText.innerText = introSlideData[0].bottomText;
+//         }
+//         else {
+//             heroImg.src = heroImg.src.replace(imgSource, "/intro/" + introSlideData[currImgIndex + 1].picture);
+//             heroImg.alt = introSlideData[0].picture.split(".")[1];
+//             topText.innerText = introSlideData[currImgIndex + 1].topText;
+//             bottomText.innerText = introSlideData[currImgIndex + 1].bottomText;
+//         }
+//     } 
+//     else {
+//         if(currentImg === introSlideData[0].picture) {
+//             heroImg.src = heroImg.src.replace(imgSource, "/intro/" + introSlideData[introSlideData.length-1].picture);
+//             heroImg.alt = introSlideData[0].picture.split(".")[1];
+//             topText.innerText = introSlideData[introSlideData.length-1].topText;
+//             bottomText.innerText = introSlideData[introSlideData.length-1].bottomText;
+//         } 
+//         else {
+//             heroImg.src = heroImg.src.replace(imgSource, "/intro/" + introSlideData[currImgIndex - 1].picture);
+//             heroImg.alt = introSlideData[0].picture.split(".")[1];
+//             topText.innerText = introSlideData[currImgIndex - 1].topText;
+//             bottomText.innerText = introSlideData[currImgIndex - 1].bottomText;
+//         }
+//     }
+// }
